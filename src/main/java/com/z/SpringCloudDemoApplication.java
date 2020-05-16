5fae68aa486dc4896785906be79d2984018b0781
walter.zhou <z67268651@gmail.com>
2020-05-16 09:35:49 +0800

    init

---
+++ src/main/java/com/z/SpringCloudDemoApplication.java
@@ -0,0 +1,13 @@
+package com.z;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudDemoApplication.class, args);
+	}
+
+}

872911704a8df9d4aae234473f94927c56a3996d
walter.zhou <z67268651@gmail.com>
2020-05-23 13:17:05 +0800

    add sql config

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # spring-cloud-demo
 spring cloud demo
+
+# How to run
+run com.z.SpringCloudDemoApplication
--- src/main/java/com/z/controller/TestController.java
+++ src/main/java/com/z/controller/TestController.java
@@ -1,13 +1,17 @@
 package com.z.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    @Value("${app.env}")
+    private String env;
+
     @GetMapping("/test")
     public String test() {
-        return "Hello World2222";
+        return env + " Hello World2222";
     }
 }

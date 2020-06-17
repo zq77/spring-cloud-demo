3c0851858716cce89508a2195bccfe3757b4ae45
z <z67268651@gmail.com>
2020-06-17 23:20:18 +0800

    add eureka

--- eureka/src/test/java/com/z/eureka/EurekaApplicationTests.java
+++ eureka/src/test/java/com/z/eureka/EurekaApplicationTests.java
@@ -1,13 +1,13 @@
-package com.z;
+package com.z.eureka;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringCloudDemoApplicationTests {
+class EurekaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }

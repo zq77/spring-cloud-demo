ecb19a16101136b0b4418c965542da95037c6ce6
Walter Zhou <z13903417792@163.com>
2023-02-01 20:50:07 +0800

    update

--- product/server/src/main/java/com/z/product/controller/MsgController.java
+++ product/server/src/main/java/com/z/product/controller/MsgController.java
@@ -1,6 +1,8 @@
 package com.z.product.controller;
 
+import com.z.product.view.ProductView;
 import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.integration.support.MessageBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +15,10 @@ public class MsgController {
 
     @GetMapping("/msg/send")
     public void sendMsg() {
-        streamBridge.send("myStream-out-0", "test stream send");
+//        streamBridge.send("myStream-out-0", "test stream send");
+        ProductView view = new ProductView();
+        view.setId(12131L);
+        streamBridge.send("myStreamOutput-out-0", MessageBuilder.withPayload(view).build());
+
     }
 }
--- product/server/src/main/java/com/z/product/message/StreamReceiver.java
+++ product/server/src/main/java/com/z/product/message/StreamReceiver.java
@@ -1,8 +1,11 @@
 package com.z.product.message;
 
+import com.z.product.model.Product;
+import com.z.product.view.ProductView;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Component;
 
 import java.util.function.Consumer;
@@ -12,10 +15,18 @@ public class StreamReceiver {
 
     private Logger LOG = LoggerFactory.getLogger(StreamReceiver.class);
 
+//    @Bean
+//    Consumer<String> myStreamInput() {
+//        return str -> {
+//            LOG.info("myStreamInput: " + str);
+//        };
+//    }
+
     @Bean
-    Consumer<String> myStreamInput() {
-        return str -> {
-            LOG.info("myStreamInput: " + str);
+    Consumer<ProductView> myStreamInput() {
+        return view -> {
+            LOG.info("myStreamInput: " + view.toString());
+            LOG.info("myStreamInput: " + view.getId());
         };
     }
 }
